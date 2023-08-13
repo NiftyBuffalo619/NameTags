be0b8a36723feca12f97c969aff09d129bed220b
NiftyBuffalo619 <polaclukes@outlook.cz>
2023-08-13 17:05:18 +0200

    New Commit

--- src/main/java/wannabenifty/nametags/events/ServerEventHandler.java
+++ src/main/java/wannabenifty/nametags/events/ServerEventHandler.java
@@ -1,25 +1,69 @@
 package wannabenifty.nametags.events;
 
-import com.lkeehl.tagapi.TagAPI;
-import com.lkeehl.tagapi.TagBuilder;
-import com.lkeehl.tagapi.api.Tag;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Team;
+import wannabenifty.nametags.NameTags;
 
 public class ServerEventHandler implements Listener {
     @EventHandler
     public static void onPlayerJoinEvent(PlayerJoinEvent event) {
-        SetupPlayerTag(event.getPlayer());
+        Player player = event.getPlayer();
+
+        String tag = ChatColor.YELLOW + "[RANK]" + ChatColor.RESET + player.getName();
+        setPlayerNameTagTab(player , tag);
+        createFloatingNameTag(player);
+    }
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        // Remove the armor stand when the player quits
+        removeFloatingNameTag(player);
     }
 
+    private static void setPlayerNameTagTab(Player player , String tag) {
+        Team team = player.getScoreboard().getPlayerTeam(player);
+        if (team == null) {
+            team = player.getScoreboard().registerNewTeam(player.getName());
+        }
+        team.setPrefix(tag);
+        team.addEntry(player.getName());
+    }
 
-    private static void SetupPlayerTag(Player player) {
-        TagBuilder builder = TagBuilder.create(player);
-        builder.withLine(player1 -> ChatColor.YELLOW + "[RANK]" + ChatColor.RESET + player.getName());
-        builder.build();
-        System.out.println("[NameTags]: Successfully added Rank to player " + player.getName() + ".");
+    private static void createFloatingNameTag(Player player) {
+        ArmorStand armorStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
+        armorStand.setCustomName(ChatColor.YELLOW + "[RANK]" + ChatColor.RESET + player.getName());
+        armorStand.setCustomNameVisible(true);
+        armorStand.setInvisible(true);
+        armorStand.setMarker(true);
+
+        // Update the armor stand's location periodically to follow the player
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (!player.isOnline() || player.isDead()) {
+                    armorStand.remove();
+                    cancel();
+                    return;
+                }
+                armorStand.teleport(player.getLocation().add(0, 2.2, 0));
+            }
+        }.runTaskTimer(new NameTags(), 0, 10);
+    }
+
+    private void removeFloatingNameTag(Player player) {
+        // Find and remove the armor stand associated with the player
+        player.getWorld().getEntitiesByClass(ArmorStand.class).forEach(armorStand -> {
+            if (armorStand.getCustomName() != null && armorStand.getCustomName().contains(player.getName())) {
+                armorStand.remove();
+            }
+        });
     }
 }
