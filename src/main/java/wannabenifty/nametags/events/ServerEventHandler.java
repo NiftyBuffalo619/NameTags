77320fdf807a2f05e3797eaa64202ba32700b74c
NiftyBuffalo619 <polaclukes@outlook.cz>
2023-08-13 17:38:38 +0200

    Updated ServerEventHandler

--- src/main/java/wannabenifty/nametags/events/ServerEventHandler.java
+++ src/main/java/wannabenifty/nametags/events/ServerEventHandler.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Team;
@@ -17,7 +18,7 @@ public class ServerEventHandler implements Listener {
     public static void onPlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        String tag = ChatColor.YELLOW + "[RANK]" + ChatColor.RESET + player.getName();
+        String tag = ChatColor.YELLOW + "[RANK]";
         setPlayerNameTagTab(player , tag);
         createFloatingNameTag(player);
     }
@@ -26,6 +27,29 @@ public class ServerEventHandler implements Listener {
         Player player = event.getPlayer();
         // Remove the armor stand when the player quits
         removeFloatingNameTag(player);
+    }
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        // Update the armor stand's position to follow the player
+        updateFloatingNameTagPosition(player);
+    }
+    private static void updateFloatingNameTagPosition(Player player) {
+        // Find the armor stand associated with the player
+        ArmorStand armorStand = findFloatingNameTag(player);
+        if (armorStand != null) {
+            // Update the armor stand's position to follow the player
+            armorStand.teleport(player.getLocation().add(0, 2.2, 0));
+        }
+    }
+    private static ArmorStand findFloatingNameTag(Player player) {
+        // Search for the armor stand associated with the player
+        for (ArmorStand armorStand : player.getWorld().getEntitiesByClass(ArmorStand.class)) {
+            if (armorStand.getCustomName() != null && armorStand.getCustomName().contains(player.getName())) {
+                return armorStand;
+            }
+        }
+        return null; // No matching armor stand found
     }
 
     private static void setPlayerNameTagTab(Player player , String tag) {
@@ -53,9 +77,9 @@ public class ServerEventHandler implements Listener {
                     cancel();
                     return;
                 }
-                armorStand.teleport(player.getLocation().add(0, 2.2, 0));
+                armorStand.teleport(player.getLocation().add(0, 1.8, 0));
             }
-        }.runTaskTimer(new NameTags(), 0, 10);
+        }.runTaskTimer(new NameTags(), 0, 1);
     }
 
     private void removeFloatingNameTag(Player player) {
