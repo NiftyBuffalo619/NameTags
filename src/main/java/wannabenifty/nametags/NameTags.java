ac9c0ffcca665823691d275d23b0554e36379826
NiftyBuffalo619 <polaclukes@outlook.cz>
2023-08-13 14:43:15 +0200

    First Commit

---
+++ src/main/java/wannabenifty/nametags/NameTags.java
@@ -0,0 +1,16 @@
+package wannabenifty.nametags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NameTags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
