e63133e819f77c3b81f7eca33e0247b3fbf51ee8
NiftyBuffalo619 <polaclukes@outlook.cz>
2023-08-15 16:08:21 +0200

    New Tag System Config ⚙

--- src/wannabenifty/nametags/Main.java
+++ src/wannabenifty/nametags/Main.java
@@ -2,21 +2,25 @@ package wannabenifty.nametags;
 
 import com.lkeehl.tagapi.TagAPI;
 import org.bukkit.Tag;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import wannabenifty.nametags.events.Events;
 
 public class Main extends JavaPlugin {
-
+    FileConfiguration config = this.getConfig();
     @Override
     public void onEnable() {
         TagAPI.onEnable(this);
         System.out.println("[NameTags]: Plugin has successfully started!");
         getServer().getPluginManager().registerEvents(new Events() , this);
+        this.getConfig();
+        saveConfig();
     }
 
     @Override
     public void onDisable() {
         TagAPI.onDisable();
+        saveConfig();
         System.out.println("[NameTags]: Plugin was successfully disabled!");
     }
 }
--- src/wannabenifty/nametags/events/Events.java
+++ src/wannabenifty/nametags/events/Events.java
@@ -2,18 +2,45 @@ package wannabenifty.nametags.events;
 
 import com.lkeehl.tagapi.TagBuilder;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import wannabenifty.nametags.Main;
 
 public class Events implements Listener {
+
     @EventHandler
     public static void onPlayerJoinEvent(PlayerJoinEvent event) {
+        event.getPlayer().setDisplayName(event.getPlayer().getName());
+        String Tag;
+
+        try {
+            Main main = JavaPlugin.getPlugin(Main.class);
+            FileConfiguration config = main.getConfig();
+            String UUID = event.getPlayer().getUniqueId().toString();
+            Tag = config.getString(UUID + ".tag");
+            System.out.println(event.getPlayer().getUniqueId().toString() + ".tag");
+            if (Tag == null || Tag.isEmpty()) {
+                Tag = "[Beginner]";
+                System.out.println("[NameTags]: No rank found.");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("[NameTags]: There was an error while loading the config file.");
+            System.out.println("[NameTags]: Error message : " + e.getStackTrace());
+            Tag = "";
+        }
+        final String finalTag = Tag;
+        System.out.println("[NameTags]: Adding rank " + finalTag + " to player " + event.getPlayer().getName() + ".");
+        event.getPlayer().setDisplayName("");
+        event.getPlayer().setDisplayName(ChatColor.YELLOW + finalTag + ChatColor.RESET + event.getPlayer().getName());
+        event.getPlayer().setPlayerListName(ChatColor.YELLOW + finalTag + ChatColor.RESET + event.getPlayer().getName());
         TagBuilder builder = TagBuilder.create(event.getPlayer());
 
-        event.getPlayer().setPlayerListName(ChatColor.YELLOW + "[RANK] " + ChatColor.RESET + event.getPlayer().getName());
-        builder.withLine(player -> ChatColor.YELLOW + "[RANK] " + ChatColor.RESET + event.getPlayer().getName());
+        builder.withLine(player -> ChatColor.YELLOW + finalTag + ChatColor.RESET + event.getPlayer().getName());
         builder.build();
-        System.out.println("[NameTags]: Successfully added tag to Player " + event.getPlayer().getName() + "with UUID " + event.getPlayer().getUniqueId());
+        System.out.println("[NameTags]: Successfully added tag to Player " + event.getPlayer().getName() + " with UUID " + event.getPlayer().getUniqueId());
     }
 }
